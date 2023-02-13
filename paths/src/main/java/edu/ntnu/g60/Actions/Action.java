b48d21ce78139cc7842919eec531ae479c5f486d
olavsro <olavsie@hotmail.com>
2023-02-14 00:07:20 +0100

    Added annotation to classes

--- paths/src/main/java/edu/ntnu/g60/Actions/Action.java
+++ paths/src/main/java/edu/ntnu/g60/Actions/Action.java
@@ -2,8 +2,8 @@ package edu.ntnu.g60.Actions;
 
 import edu.ntnu.g60.Player;
 
+@FunctionalInterface
 interface Action {
-    
     public void execute(Player player);
 }
 
--- paths/src/main/java/edu/ntnu/g60/Goals/Goal.java
+++ paths/src/main/java/edu/ntnu/g60/Goals/Goal.java
@@ -2,9 +2,9 @@ package edu.ntnu.g60.Goals;
 
 import edu.ntnu.g60.Player;
 
+@FunctionalInterface
 interface Goal{
   public boolean isFulfilled(Player player);
-
 }
 
 
