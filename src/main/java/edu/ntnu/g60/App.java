663bb51e42beb9f5537a3de4a747642af40a2174
Stianlyng <stianlyng@protonmail.com>
2023-04-15 17:42:59 +0200

    hardcode game

--- src/main/java/edu/ntnu/g60/App.java
+++ src/main/java/edu/ntnu/g60/App.java
@@ -17,20 +17,22 @@ public class App {
     public static void main(String[] args) {
         
         Passage openingPassage = new Passage("Opening Passage", "This is the opening passage");
+        
+        Story story = new Story("Haunted House", openingPassage);
+        
+        Passage firstPassage = new Passage("passage0", "content0");
+        story.addPassage(firstPassage);
         Link link = new Link("forward", "passage0");
         openingPassage.addLink(link);
 
-        Story story = new Story("Haunted House", openingPassage);
-        
-        for (int i = 0; i < 10;i++) {
+        for (int i = 1; i < 10; i++) {
             Passage p = new Passage("passage" + i, "content" + i);
             story.addPassage(p);
-            Link linkForward = new Link("forward", "passage" + (i+1));
-            Link linkBack = new Link("backwards", "passage" + (i-1));
+            Link linkForward = new Link("forward", "passage" + (i + 1));
+            Link linkBack = new Link("backwards", "passage" + (i - 1));
             p.addLink(linkForward);
             p.addLink(linkBack);
-        }   
-
+        }
 
         List<Goal> goals = List.of( 
                     new HealthGoal(0), 
@@ -45,28 +47,18 @@ public class App {
         game.begin();
 
         System.out.println(game.getStory().getOpeningPassage().toString());
-        System.out.println(game.getStory().getOpeningPassage().getLinks().get(0));
+        //System.out.println(game.getStory().getOpeningPassage().getLinks().get(0));
 
         Link nextLink = game.getStory().getOpeningPassage().getLinks().get(0);
+        //System.out.println(nextLink);
+        Link nextLink2 = new Link("forward", "passage0");
 
+        // check if the link equals the reference
+        System.out.println(nextLink.equals(nextLink2));
         Passage pas = game.go(nextLink);
 
         System.out.println(pas);
 
-        /*
-        System.out.println("Story title: " + story.getTitle());
-        
-        Passage getOpeningPassage = story.getOpeningPassage();
-        System.out.println("Opening passage title: " + getOpeningPassage.getTitle());
-        System.out.println("Opening passage content: " + getOpeningPassage.getContent());
-        System.out.println("Opening passage links: " + getOpeningPassage.getLinks());
-        
-        story.getPassages().forEach((passage) -> {
-            System.out.println("Passage title: " + passage.getTitle());
-            System.out.println("Passage content: " + passage.getContent());
-            System.out.println("Passage links: " + passage.getLinks());
-        });
-        */
-        
+        //System.out.println(game.getStory().getOpeningPassage().getLinks().get(0));
     }
 }
