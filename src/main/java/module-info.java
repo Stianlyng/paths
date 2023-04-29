7b2f919348347da2d52f034ef8f644b782dee207
Stianlyng <stianlyng@protonmail.com>
2023-04-29 14:40:38 +0200

    added export and requirement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@ module edu.ntnu.g60 {
     requires transitive javafx.controls;
     requires transitive javafx.graphics;
     requires transitive javafx.media;
-    
+    requires com.fasterxml.jackson.databind;
+
     opens edu.ntnu.g60.frontend to javafx.fxml;
     exports edu.ntnu.g60.frontend;
+
+    exports edu.ntnu.g60.entities to com.fasterxml.jackson.databind;
 }
