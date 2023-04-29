ea6aee9b3962c2382b9ebf84c40b08e395bf6f40
Stianlyng <stianlyng@protonmail.com>
2023-04-29 14:29:51 +0200

    Created entitites

---
+++ src/main/java/edu/ntnu/g60/entities/LinkEntity.java
@@ -0,0 +1,23 @@
+package edu.ntnu.g60.entities;
+
+public class LinkEntity {
+    private String text;
+    private String reference;
+
+    // Getters and setters
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+}
---
+++ src/main/java/edu/ntnu/g60/entities/PassageEntity.java
@@ -0,0 +1,36 @@
+package edu.ntnu.g60.entities;
+
+import java.util.List;
+
+
+public class PassageEntity {
+
+    private String title;
+    private String content;
+    private List<LinkEntity> links;
+
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<LinkEntity> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<LinkEntity> links) {
+        this.links = links;
+    }
+}
