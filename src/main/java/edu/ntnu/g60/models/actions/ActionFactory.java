5abdcaa01e3b4a79819b1cc74dc8628f3cd585c8
Stianlyng <stianlyng@protonmail.com>
2023-04-29 20:00:10 +0200

    Add optional and enum

--- src/main/java/edu/ntnu/g60/models/actions/ActionFactory.java
+++ src/main/java/edu/ntnu/g60/models/actions/ActionFactory.java
@@ -1,37 +1,44 @@
 package edu.ntnu.g60.models.actions;
 
+import java.util.Optional;
+
 import edu.ntnu.g60.entities.ActionEntity;
 
 public class ActionFactory {
 
     public static Action createAction(ActionEntity actionEntity) {
-        String type = actionEntity.getType();
+        ActionTypeEnum type = ActionTypeEnum.fromString(actionEntity.getType());
         Object value = actionEntity.getValue();
 
-        switch (type.toLowerCase()) {
-            case "health":
+        return createTypedAction(type, value).orElseThrow(() ->
+                new IllegalArgumentException("Invalid action type or value: " + actionEntity.getType() + ", " + value));
+    }
+
+    private static Optional<Action> createTypedAction(ActionTypeEnum type, Object value) {
+        switch (type) {
+            case HEALTH:
                 if (value instanceof Integer) {
-                    return new HealthAction((int) value);
+                    return Optional.of(new HealthAction((int) value));
                 }
                 break;
-            case "gold":
+            case GOLD:
                 if (value instanceof Integer) {
-                    return new GoldAction((int) value);
+                    return Optional.of(new GoldAction((int) value));
                 }
                 break;
-            case "score":
+            case SCORE:
                 if (value instanceof Integer) {
-                    return new ScoreAction((int) value);
+                    return Optional.of(new ScoreAction((int) value));
                 }
                 break;
-            case "inventory":
+            case INVENTORY:
                 if (value instanceof String) {
-                    return new InventoryAction((String) value);
+                    return Optional.of(new InventoryAction((String) value));
                 }
                 break;
             default:
                 break;
         }
-        throw new IllegalArgumentException("Invalid action type or value: " + type + ", " + value);
+        return Optional.empty();
     }
 }
