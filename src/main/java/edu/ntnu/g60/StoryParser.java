cc4729e07b12870fce32cbe4290fe9dbf1436640
Stianlyng <stianlyng@protonmail.com>
2023-04-29 17:21:00 +0200

    refactor: seperate build passage

--- src/main/java/edu/ntnu/g60/StoryParser.java
+++ src/main/java/edu/ntnu/g60/StoryParser.java
@@ -11,60 +11,56 @@ import edu.ntnu.g60.models.Link;
 import edu.ntnu.g60.models.Passage;
 import edu.ntnu.g60.models.Story;
 import edu.ntnu.g60.models.StoryBuilder;
-
 import edu.ntnu.g60.models.PassageBuilder;
 
+/**
+ * StoryParser is a class that parses a JSON file into a Story object.
+ *
+ * @version 1.0
+ * @author Stian Lyng
+ */
 public class StoryParser {
-    
+
     private String jsonFilePath;
+
     private ObjectMapper objectMapper = new ObjectMapper();
-    
+
     public StoryParser(String jsonFilePath) {
         this.jsonFilePath = jsonFilePath;
     }
-    
+
     public Story build() {
         try {
-
             StoryEntity storyMap = objectMapper.readValue(new File(jsonFilePath), StoryEntity.class);
 
-            PassageEntity openingPassageMap = storyMap.getPassages().get(0);
-
-            Passage openingPassage = new PassageBuilder()
-                .setTitle(openingPassageMap.getTitle())
-                .setContent(openingPassageMap.getContent())
-                .build();
-            
-            openingPassageMap.getLinks().forEach(link -> {
-                openingPassage.addLink(new Link(link.getText(), link.getReference()));
-            });
-
-            Story story = new StoryBuilder()
-                .setTitle(storyMap.getTitle())
-                .setOpeningPassage(openingPassage)
-                .build();
-
-
+            StoryBuilder storyBuilder = new StoryBuilder()
+                .setTitle(storyMap.getTitle());
 
             storyMap.getPassages().forEach(passageMap -> {
-
-                Passage passage = new PassageBuilder()
-                    .setTitle(passageMap.getTitle())
-                    .setContent(passageMap.getContent())
-                    .build();
-                
-                passageMap.getLinks().forEach(link -> {
-                    passage.addLink(new Link(link.getText(), link.getReference()));
-                });
-
-                story.addPassage(passage);
+                Passage passage = buildPassage(passageMap);
+                storyBuilder.addPassage(passage);
+                if (storyMap.getPassages().indexOf(passageMap) == 0) {
+                    storyBuilder.setOpeningPassage(passage);
+                }
             });
 
-            return story;
+            return storyBuilder.build();
         } catch (IOException e) {
             e.printStackTrace();
-
             return null;
         }
+    }
+
+    private Passage buildPassage(PassageEntity passageMap) {
+        Passage passage = new PassageBuilder()
+            .setTitle(passageMap.getTitle())
+            .setContent(passageMap.getContent())
+            .build();
+
+        passageMap.getLinks().forEach(link -> {
+            passage.addLink(new Link(link.getText(), link.getReference()));
+        });
+
+        return passage;
     }
 }
