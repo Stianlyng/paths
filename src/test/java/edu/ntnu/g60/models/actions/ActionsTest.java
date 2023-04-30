a55e3dd4b7b272d6c05630fdce21f6a25d7fb582
Stianlyng <stianlyng@protonmail.com>
2023-04-30 19:44:50 +0200

    add test for actions

---
+++ src/test/java/edu/ntnu/g60/models/actions/ActionsTest.java
@@ -0,0 +1,85 @@
+package edu.ntnu.g60.models.actions;
+
+
+import edu.ntnu.g60.exceptions.model.actions.IllegalActionException;
+import edu.ntnu.g60.models.player.Player;
+import edu.ntnu.g60.models.player.PlayerBuilder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+/**
+ * Test class for all the actionis.
+ * 
+ * @author Stian Lyng
+ */
+public class ActionsTest {
+
+    private Player player;
+
+    /**
+     * Creates a new player before each test.
+     */
+    @BeforeEach
+    public void setUp() {
+        player = new PlayerBuilder()
+                .setName("s1mple")
+                .setHealth(0)
+                .setScore(0)
+                .setGold(0)
+                .build();
+    }
+
+    /**
+     * Checks that the player's health is increased by the value provided.
+     * @throws IllegalActionException if the player is null.
+     */
+    @Test
+    public void test_gold_action() throws IllegalActionException {
+        GoldAction goldAction = new GoldAction(50);
+        goldAction.execute(player);
+        assertEquals(50, player.getGold());
+        assertThrows(IllegalActionException.class, () -> goldAction.execute(null));
+    }
+
+    /**
+     * Checks that the player's health is increased by the value provided.
+     * @throws IllegalActionException if the player is null.
+     */
+    @Test
+    public void test_health_action() throws IllegalActionException {
+        HealthAction healthAction = new HealthAction(10);
+        healthAction.execute(player);
+        assertEquals(10, player.getHealth());
+        assertThrows(IllegalActionException.class, () -> healthAction.execute(null));
+    }
+
+    /**
+     * Checks that the player's health is increased by the value provided.
+     * @throws IllegalActionException if the player is null.
+     */
+    @Test
+    public void test_inventory_action() throws IllegalActionException {
+        InventoryAction inventoryAction = new InventoryAction("Sword");
+        inventoryAction.execute(player);
+        assertEquals(1, player.getInventory().size());
+        assertEquals("Sword", player.getInventory().get(0));
+        assertThrows(IllegalActionException.class, () -> inventoryAction.execute(null));
+    }
+
+    /**
+     * Checks that the player's health is increased by the value provided.
+     * @throws IllegalActionException if the player is null.
+     */
+    @Test
+    public void test_score_action() throws IllegalActionException {
+        ScoreAction scoreAction = new ScoreAction(100);
+        scoreAction.execute(player);
+        assertEquals(100, player.getScore());
+        assertThrows(IllegalActionException.class, () -> scoreAction.execute(null));
+    }
+}
