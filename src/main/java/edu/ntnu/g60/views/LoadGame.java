42a8650503eeb64ac43e042280948919df298bcb
Stian Lyng <stianlyng@protonmail.com>
2023-05-19 05:48:01 +0200

    refacor load game

--- src/main/java/edu/ntnu/g60/views/LoadGame.java
+++ src/main/java/edu/ntnu/g60/views/LoadGame.java
@@ -24,11 +24,20 @@ public class LoadGame extends StackPane{
 
     public LoadGame(String playerName) {
 
-        ComboBox<String> storySelection = new ComboBox<>();
-        storySelection.setPromptText("Select a save");
-        Set<String> saves = SaveFileHandler.getPlayerSaves(playerName);
-        storySelection.getItems().addAll(saves);
+        ComboBox<String> storySelection = selectStory(playerName);
+        Button playButton = playGame(storySelection);
+        
+        GridPane layout = new GridPane();
+        layout.setAlignment(Pos.CENTER);
+        layout.setVgap(10);
+        layout.setPadding(new Insets(PADDING, PADDING, PADDING, PADDING));
+        layout.add(new Label("Welcome"), 0, 0);
+        layout.add(storySelection, 0, 1);
+        layout.add(playButton, 0, 2);
+        this.getChildren().add(layout);
+    }
 
+    private Button playGame(ComboBox<String> storySelection){
         Button playButton = new Button("Continue Story");
         playButton.setPrefWidth(BUTTON_WIDTH);
         playButton.setOnAction(e -> {
@@ -43,16 +52,15 @@ public class LoadGame extends StackPane{
             Passage currentPassage = GameManager.getInstance().getGame().go(gameState.getCurrentLink()); 
             switchToPlayGame(currentPassage);
         });
-
-        GridPane layout = new GridPane();
-        layout.setAlignment(Pos.CENTER);
-        layout.setVgap(10);
-        layout.setPadding(new Insets(PADDING, PADDING, PADDING, PADDING));
-        layout.add(new Label("Welcome"), 0, 0);
-        layout.add(storySelection, 0, 1);
-        layout.add(playButton, 0, 2);
-        this.getChildren().add(layout);
-
+        return playButton;
+    }
+    
+    private ComboBox<String> selectStory(String playerName){
+        ComboBox<String> storySelection = new ComboBox<>();
+        storySelection.setPromptText("Select a save");
+        Set<String> saves = SaveFileHandler.getPlayerSaves(playerName);
+        storySelection.getItems().addAll(saves);
+        return storySelection;
     }
 
     private void switchToPlayGame(Passage currentPassage) {
